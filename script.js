0a7239bee99517cb7ef5f363709871fab2e8c807
Maicon Rodrigues <134792626+xMikeDeveloper@users.noreply.github.com>
2024-12-07 17:15:41 -0300

    alternado

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const targetTime = new Date().getTime() + 10 * 1000;
         const remainingTime = targetTime - now;
 
         if (remainingTime <= 0) {
-            window.location.href = "site.html"; // Redireciona para o site
+            window.location.href = "site/site.html"; // Redireciona para o site
         } else {
             const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
             document.getElementById("countdown-timer").textContent = `${seconds}s`;
